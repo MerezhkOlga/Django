9418ed4ad95d1dffe90114abcff5b4c1da5e2ce0
MerezgkOlga <MerezgkOly@gmail.com>
2021-12-17 02:46:36 +0300

    HW 2

--- lesson_1/geekshop/geekshop/urls.py
+++ lesson_1/geekshop/geekshop/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import index, contacts
+from .views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
-    path('contacts/', contacts),
-    path('products/', include('mainapp.urls')),
+    path('', index, name='index'),
+    path('contacts/', contacts, name='contacts'),
+    path('products/', include('mainapp.urls', namespace='products')),
 ]
