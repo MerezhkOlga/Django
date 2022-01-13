30f939445fcb03872604baae1796a3d4a6a74737
MerezgkOlga <MerezgkOly@gmail.com>
2022-01-13 22:01:35 +0300

    HW 6

--- geekshop/geekshop/views.py
+++ geekshop/geekshop/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 
+from basketapp.models import Basket
 from mainapp.models import Product
 
 
 def index(request):
     title = 'GeekShop'
     products = Product.objects.all()[:4]
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
     context = {
         'title': title,
         'products': products,
+        'basket': basket,
     }
     return render(request, 'geekshop/index.html', context)
 
@@ -19,4 +24,3 @@ def contacts(request):
         'title': title,
     }
     return render(request, 'geekshop/contacts.html', context)
-
