6d4e27b633852dff0d7a5c3b312b5d6e0d7bdf6f
MerezgkOlga <MerezgkOly@gmail.com>
2022-01-20 14:13:20 +0300

    HW 7

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -8,6 +8,8 @@ class ProductCategory(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    is_active = models.BooleanField(default=True)
+
     def __str__(self):
         return self.name
 
@@ -20,6 +22,8 @@ class Product(models.Model):
     description = models.TextField(verbose_name='описание', blank=True)
     price = models.DecimalField(verbose_name='цена продукта', max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(verbose_name='количество на складе', default=0)
+
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return f'{self.name} ({self.category.name})'
